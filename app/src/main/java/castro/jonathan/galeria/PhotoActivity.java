d097b18434ea1d5f8a2aed132348461d79a45f40
20201TIIMI0101 <jonathancastrosilva8@gmail.com>
2022-09-16 09:57:38 -0300

    CABEI na parte 13 kkkkkkk vinicius pra sempre

--- app/src/main/java/castro/jonathan/galeria/PhotoActivity.java
+++ app/src/main/java/castro/jonathan/galeria/PhotoActivity.java
@@ -4,14 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ImageView;
+
+import java.io.File;
 
 
 public class PhotoActivity extends AppCompatActivity {
@@ -56,4 +60,14 @@ public class PhotoActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    void sharePhoto() {
+        // Codigo para compartiilhar a foto
+        Uri photoUri = FileProvider.getUriForFile(PhotoActivity.this, "castro.jonathan.galeria.fileprovider", new File(photoPath));
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.putExtra(Intent.EXTRA_STREAM, photoUri);
+        i.setType("image/jpeg");
+        startActivity(i);
+    }
+
 }
